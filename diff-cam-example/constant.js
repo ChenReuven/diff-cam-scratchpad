49c8d5fd6fdca596d507f793b2e7caf052ae87da
reuvec <chen.reuven@dell.com>
2018-10-09 11:56:38 +0300

    fixes

--- diff-cam-example/constant.js
+++
@@ -1,5 +0,0 @@
-const constant = (function(){
-  return {
-    START_CHANGE_DETECTION_SENSITIVITY: 900
-}
-}());
--- diff-cam-example/site.js
+++ diff-cam-example/site.js
@@ -1,4 +1,3 @@
-const constantObj = constant;
 const video = document.getElementById('video');
 const score = document.getElementById('score');
 const body = document.getElementById('body');
@@ -6,6 +5,17 @@ const sound = new Howl({
 	src: ['sound-dog.mp3']
 });
 
+// Slider
+const slider = document.getElementById("myRange");
+const output = document.getElementById("demo");
+let {value: sliderValue} = slider;
+output.innerHTML = sliderValue;
+slider.oninput = function () {
+	output.innerHTML = this.value;
+	sliderValue = this.value;
+}
+
+// Diff Cam
 function initSuccess() {
 	DiffCamEngine.start();
 }
@@ -19,20 +29,21 @@ function isSoundPlaying(sound) {
 	return sound.playing();
 }
 
-function startTime(){
+function startTime() {
 	body.classList.toggle('red');
 }
 
 function flickeringScreen() {
-	const tt = setInterval(function(){ startTime() }, 200);
-	setTimeout(function() { clearInterval(tt); body.classList.remove('red')}, 10000);
+	const tt = setInterval(function () { startTime() }, 200);
+	setTimeout(function () { clearInterval(tt); body.classList.remove('red') }, 10000);
 }
 
 function capture(payload) {
 	score.textContent = payload.score;
 	const scoreResult = payload.score;
-	if(scoreResult > constantObj.START_CHANGE_DETECTION_SENSITIVITY) {
-		if(!isSoundPlaying(sound)) {
+	console.log('sliderValue = ', sliderValue);
+	if (scoreResult > sliderValue) {
+		if (!isSoundPlaying(sound)) {
 			sound.play();
 			flickeringScreen();
 		}
