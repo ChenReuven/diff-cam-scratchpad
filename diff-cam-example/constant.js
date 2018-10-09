955fc7c905223637f6061858917f2d1d58794680
chenreuven <chen.rvn@gmail.com>
2018-10-09 07:32:14 +0300

    add detection capability and sound

---
+++ diff-cam-example/constant.js
@@ -0,0 +1,5 @@
+const constant = (function(){
+  return {
+    START_CHANGE_DETECTION_SENSITIVITY: 900
+}
+}());
--- diff-cam-example/site.js
+++ diff-cam-example/site.js
@@ -1,23 +1,46 @@
-var video = document.getElementById('video');
-//var canvas = document.getElementById('motion');
-var score = document.getElementById('score');
+const constantObj = constant;
+const video = document.getElementById('video');
+const score = document.getElementById('score');
+const body = document.getElementById('body');
+const sound = new Howl({
+	src: ['sound-dog.mp3']
+});
 
 function initSuccess() {
 	DiffCamEngine.start();
 }
 
-function initError() {
+function initError(e) {
 	alert('Something went wrong.');
+	console.log("Init error:" + e);
+}
+
+function isSoundPlaying(sound) {
+	return sound.playing();
+}
+
+function startTime(){
+	body.classList.toggle('red');
+}
+
+function flickeringScreen() {
+	const tt = setInterval(function(){ startTime() }, 200);
+	setTimeout(function() { clearInterval(tt); body.classList.remove('red')}, 10000);
 }
 
 function capture(payload) {
-	console.log("payload = ", payload);
 	score.textContent = payload.score;
+	const scoreResult = payload.score;
+	if(scoreResult > constantObj.START_CHANGE_DETECTION_SENSITIVITY) {
+		if(!isSoundPlaying(sound)) {
+			sound.play();
+			flickeringScreen();
+		}
+	}
 }
 
 DiffCamEngine.init({
 	video: video,
-	//motionCanvas: canvas,
 	initSuccessCallback: initSuccess,
 	initErrorCallback: initError,
 	captureCallback: capture
